3f045ffab2f793a5295a8f3ba36af3bed39ff5d7
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-09-06 01:24:34 +0700

    adjust BannerImage component's Image position

--- src/app/components/BannerImage.js
+++ src/app/components/BannerImage.js
@@ -35,8 +35,8 @@ export default function BannerImage() {
     //   </div>
     // </section>
     <section className="relative bg-[#0E3179] h-screen max-h-[1440px] md:h-[1028px] w-full flex overflow-hidden">
-      <Image src={BannerImageXochilt} alt="banner-image" className="absolute top-5 md:top-24 md:left-10" width={700}/>
-      <div className="absolute bottom-20 left-0 right-0 w-full flex flex-col items-start md:items-end px-10 md:px-28 md:pb-16 gap-4">
+      <Image src={BannerImageXochilt} alt="banner-image" className="absolute top-20 md:top-24 md:left-10" width={700}/>
+      <div className="absolute bottom-10 md:bottom-96 left-0 right-0 w-full flex flex-col items-start md:items-end px-10 md:px-28 md:pb-16 gap-4">
         <h1 className="max-w-[470px] break-words flex flex-col justify-center font-white font-resolute tracking-[2px] leading-tight font-normal capitalize text-center">
           <span className="text-[30px] md:text-[65px]">JUNTOS POR</span>
           <span className="text-[34px] md:text-[73px]">UN MEXICO</span>
