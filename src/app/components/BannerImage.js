05474b1e2e749e9d18fdca1d9af1a656311573f6
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-09-06 13:46:07 +0700

    apply font-resolute on BannerImage component

--- src/app/components/BannerImage.js
+++ src/app/components/BannerImage.js
@@ -34,11 +34,11 @@ export default function BannerImage() {
     //     />
     //   </div>
     // </section>
-    <section className="relative bg-[color:var(--secondary-color)] h-screen max-h-[1440px] md:h-[1028px] w-full flex overflow-hidden">
+    <section className="relative bg-[color:var(--secondary-color)] font-resolute h-screen max-h-[1440px] md:h-[1028px] w-full flex overflow-hidden">
       <Image src={BannerImageXochilt} alt="banner-image" className="absolute top-20 md:top-24 md:left-10" width={700}/>
       <div className="absolute bottom-10 lg:bottom-96 left-0 right-0 w-full flex flex-col items-start md:items-end px-10 md:px-28 md:pb-16 gap-4">
         <div className="flex flex-col gap-5 md:gap-10">
-          <h1 className="max-w-[470px] break-words flex flex-col justify-center font-white font-resolute tracking-[2px] leading-tight font-normal capitalize text-center">
+          <h1 className="max-w-[470px] break-words flex flex-col justify-center font-white tracking-[2px] leading-tight font-normal capitalize text-center">
             <span className="text-[30px] md:text-[65px]">JUNTOS POR</span>
             <span className="text-[34px] md:text-[73px]">UN MEXICO</span>
             <span className="text-[34px] md:text-[73px]">SIN LIMITES</span>
