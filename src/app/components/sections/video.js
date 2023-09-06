8d35a022a01ce48e95515705bd49b5d7686d1705
hellofromiqbal <120085191+hellofromiqbal@users.noreply.github.com>
2023-09-06 22:15:38 +0700

    Adjust video section (#16)

--- src/app/components/sections/video.js
+++ src/app/components/sections/video.js
@@ -4,12 +4,12 @@ export default function VideoSection() {
   // const linkVideo = "https://mega.nz/embed/lyt1zBDA#oXLwhWOgNly83QKydaPKcMmlrG2b6UB2twkVms0EkbU!1a1m"
 
   return(
-    <section className="relative w-full lg:h-[667px] flex flex-col md:flex-row-reverse bg-[color:var(--secondary-color)]">
-      <div className="md:absolute flex px-2 md:p-0 -top-10 md:-top-10 md:right-16 lg:right-20 md:w-[350px] lg:w-[600px] h-[398px] md:h-[400px] lg:h-[680px] bg-white">
+    <section className="relative w-full md:mt-20 md:h-[667px] lg:h-[500px] flex flex-col md:flex-row-reverse bg-[color:var(--secondary-color)]">
+      <div className="md:absolute flex px-2 md:p-0 -top-10 md:-top-20 lg:-top-40 md:w-[400px] lg:w-[650px] h-[398px] md:h-[400px] lg:h-[500px] bg-white">
         <video src={"./video/xochitlvideo1.mp4"} className='w-full h-full object-cover' autoPlay loop muted/>
       </div>
-      <div className="basis-1/2 md:basis-full flex justify-start items-center p-10 md:p-20">
-        <div className="md:basis-5/12">
+      <div className="basis-1/2 md:basis-full flex justify-start md:items-center lg:items-start p-10 md:p-20">
+        <div className="md:basis-8/12 lg:basis-5/12 md:pt-10 lg:pt-0">
           <TitleCaptionText
             title={'SIN MIEDO'}
             title2={'A NADA'}
