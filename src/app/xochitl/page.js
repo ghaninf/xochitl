e2b9d0a7c5b6f4a0f6aa61ecfecc315ac75eb62f
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-09-07 14:16:31 +0700

    add generateContentItem function

--- src/app/xochitl/page.js
+++ src/app/xochitl/page.js
@@ -18,33 +18,79 @@ export default function Meets() {
     { src: InstagramIcon, title: "instagram-icon", width: 22.03, height: 22.03 },
   ];
 
-  const articles = [
+  const content = [
     {
+      type: 'article',
       title: "UNA VIDA DE COMPROMISO SOCIAL Y TECNOLOGICO",
       excerpt: 'Xóchitl es una mujer que toda su vida se ha enfrentado y ha luchado contra la desigualdad en México, y ha podido salir adelante a base esfuerzo, valentía y determinación.'
     },
     {
+      type: 'article',
       title: "ORIGENES Y EDUCACION",
       excerpt: 'Nación en 1964 en Pachuca, Hidalgo, Xóchitl es de ascendencia otomí. Su origen indígena ha influido profundamente en su visión del mundo y en su compromiso con los derechos humanos y el bienestar de los pueblos indígenas de México. Para trasladarse a su escuela debía trasladarse todos los días y para costear sus estudios vendía gelatinas en el mercado de su pueblo. Xóchitl es ingeniera en sistemas computacionales por el Universidad Nacional Autónoma de México Politécnico Nacional (UNAM) y y posteriormente se especializó en robótica, inteligencia artificial, edificios inteligentes, sustentabilidad y ahorro de energía.'
     },
     {
+      type: 'article',
       title: "TRAYECTORIA EN EL SECTOR PRIVADO",
       excerpt: 'Xóchitl comenzó su vida laboral como programadora y continuó desempeñándose c con éxito en el ámbito de las tecnologías de la información. Fundó empresas enfocadas en ofrecer servicios informáticos y digitales, consolidando su reputación como una de las mujeres más influyentes en el sector tecnológico mexicano.'
     },
     {
+      type: 'quote',
+      quote: `"TENGO UN ENFOQUE DISTINTO PARA MEXICO, CON IDEAS MAS FRESCAS ENCAMINADAS HACIA UN MEXICO SIN LIMITES."`,
+      author: 'XOCHITL GALVEZ'
+    },
+    {
+      type: 'article',
       title: "COMPROMISO SOCIAL",
       excerpt: 'A inicios del siglo XXI, su camino tomó un giro hacia el servicio público. Iniciando en la dirección de la Comisión Nacional para el Desarrollo de los Pueblos Indígenas (CDI). Desde este cargo, impulsó proyectos que buscaban mejorar las condiciones de vida de las comunidades indígenas, reconociendo y promoviendo sus derechos y cultura. Posteriormente, ha ocupado diversos cargos públicos, incluyendo fungir como Jefa Delegacional de la Miguel Hidalgo en la Ciudad de México y Senadora de la República. En cada puesto ha buscado combinar su experiencia académica con su compromiso social, generando proyectos que integren la modernización con la inclusión y justicia buscando un México justo y sin límites.'
     },
     {
+      type: 'article',
       title: "VIDA PERSONAL",
       excerpt: 'Aunque no están casados, Xóchitl y Rubén Sánchez se dicen esposos y juntos tienen dos hijos Diana y Juan Pablo. Le va al Club de Fútbol Cruz Azul y es una fanática del ciclismo.',
+    },
+    {
+      type: 'image',
       image: VidaPersonal
     },
     {
+      type: 'article',
       title: "VISION",
       excerpt: 'Xóchitl es un claro ejemplo de que la combinación de pasión, educación y compromiso pueden generar transformaciones positivas. A través de su carrera, ha demostrado que es posible unir mundos aparentemente opuestos, como la tecnología y la tradición, para construir un México más justo e incluyente.Su historia es una inspiración para las nuevas generaciones, mostrando que con determinación y amor por lo que se hace, es posible dejar una huella profunda en la sociedad.'
     },
-  ]
+  ];
+
+  const generateContentItem = (item, index) => {
+    switch (item.type) {
+      case 'image':
+        return (
+          <article key={index} className="w-11/12 lg:w-full mx-auto lg:mx-0">
+            <Image src={item.image} alt={"image"} className="w-full object-cover"/>
+          </article>
+        )
+        break;
+      case 'quote':
+        return (
+          <article key={index}>
+            <div className="flex flex-col gap-5 px-5 md:px-10 w-10/12 mx-auto border-s-2 md:border-s-8 border-[color:var(--primary-color)]">
+              <h2 className="font-resolute text-md md:text-[27px] text-[color:var(--secondary-color)] decoration-[color:var(--primary-color)] underline underline-offset-4 md:underline-offset-[6px]">{item.quote}</h2>
+              <p className="font-appliedSans text-sm md:text-[25px] leading-[17px] md:leading-8">{item.author}</p>
+            </div>
+          </article>
+        )
+        break;
+      default:
+        return (
+          <article key={index} className="relative">
+            <div className="flex flex-col gap-5 px-10 md:px-20">
+              <h2 className="font-resolute lg:w-max text-md md:text-[27px] text-[color:var(--secondary-color)] decoration-[color:var(--primary-color)] underline underline-offset-4 md:underline-offset-[6px]">{item.title}</h2>
+              <p className="font-appliedSans text-sm md:text-[25px] leading-[17px] md:leading-8">{item.excerpt}</p>
+            </div>
+          </article>
+        )
+        break;
+    }
+  };
 
   return(
     <>
@@ -78,21 +124,7 @@ export default function Meets() {
         </section>
         <section className="relative w-full lg:w-10/12 mx-auto min-h-screen mt-14 mb-14 box-border">
           <div className="flex flex-col gap-10 md:gap-20">
-            {articles.map((article, index) => (
-              <article key={index} className="relative">
-                <div className="flex flex-col gap-5 px-10 md:px-20">
-                  <h2 className="font-resolute lg:w-max text-md md:text-[27px] text-[color:var(--secondary-color)] decoration-[color:var(--primary-color)] underline underline-offset-8 lg:underline-offset-[1rem]">{article.title}</h2>
-                  <p className="font-appliedSans text-sm md:text-[25px] leading-[17px] md:leading-8">{article.excerpt}</p>
-                </div>
-                {article.image ?
-                  <div className="w-11/12 lg:w-full mx-auto lg:mx-0 mt-10 md:mt-20">
-                    <Image src={article.image} alt={article.title} className="w-full object-cover"/>
-                  </div>
-                  :
-                  ''
-                }
-              </article>
-            ))}
+            {content.map((item, index) => generateContentItem(item, index))}
           </div>
         </section>
       </main>
