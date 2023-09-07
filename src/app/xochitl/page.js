564ba4e88995472a361ae40b127cfb34fd458046
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-09-08 01:06:10 +0700

    fix socmed icons

--- src/app/xochitl/page.js
+++ src/app/xochitl/page.js
@@ -5,10 +5,14 @@ import { generateContentItem } from "../utils";
 import { metaData } from "../metaData";
 
 import XochitlImage from '../assets/image/banner-image-2.png';
-import ShareIcon from '../assets/image/socmed/share-white-fill.png';
-import FacebookIcon from '../assets/image/socmed/facebook-white-fill.png';
-import TwitterIcon from '../assets/image/socmed/twitter-white-fill.png';
-import InstagramIcon from '../assets/image/socmed/instagram-white-fill.png';
+// import ShareIcon from '../assets/image/socmed/share-white-fill.png';
+// import FacebookIcon from '../assets/image/socmed/facebook-white-fill.png';
+// import TwitterIcon from '../assets/image/socmed/twitter-white-fill.png';
+// import InstagramIcon from '../assets/image/socmed/instagram-white-fill.png';
+import ShareIcon from '../assets/image/socmed/share-outline.svg';
+import FacebookIcon from '../assets/image/socmed/facebook-fill.svg';
+import TwitterIcon from '../assets/image/socmed/twitter-fill.svg';
+import InstagramIcon from '../assets/image/socmed/instagram-fill.svg';
 import VidaPersonal from '../assets/image/vida-personal.png';
 import ConoceImage from '../assets/image/text/image-conoce-xochitl.webp';
 
@@ -19,10 +23,10 @@ export async function generateMetadata() {
 
 export default async function Meets() {
   const socMeds = [
-    { src: ShareIcon, title: "share-icon", width: 24, height: 24 },
-    { src: FacebookIcon, title: "facebook-icon", width: 11.57, height: 22.03 },
-    { src: TwitterIcon, title: "twitter-icon", width: 25.05, height: 20.36 },
-    { src: InstagramIcon, title: "instagram-icon", width: 22.03, height: 22.03 },
+    { src: ShareIcon, title: "share-icon", href: "https://www.facebook.com"},
+    { src: FacebookIcon, title: "facebook-icon", href: "https://www.facebook.com"},
+    { src: TwitterIcon, title: "twitter-icon", href: "https://www.twitter.com"},
+    { src: InstagramIcon, title: "instagram-icon", href: "https://www.instagram.com"},
   ];
 
   const content = [
