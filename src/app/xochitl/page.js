f68686fd65ac1591bb5d3a81e64e3e52484d8ac2
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-09-07 14:23:12 +0700

    add generateContentItem in utils folder

--- src/app/xochitl/page.js
+++ src/app/xochitl/page.js
@@ -9,6 +9,7 @@ import TwitterIcon from '../assets/image/socmed/twitter-white-fill.png';
 import InstagramIcon from '../assets/image/socmed/instagram-white-fill.png';
 import VidaPersonal from '../assets/image/vida-personal.png';
 import ConoceImage from '../assets/image/text/image-conoce-xochitl.webp';
+import { generateContentItem } from "../utils";
 
 export default function Meets() {
   const socMeds = [
@@ -59,38 +60,6 @@ export default function Meets() {
       excerpt: 'Xóchitl es un claro ejemplo de que la combinación de pasión, educación y compromiso pueden generar transformaciones positivas. A través de su carrera, ha demostrado que es posible unir mundos aparentemente opuestos, como la tecnología y la tradición, para construir un México más justo e incluyente.Su historia es una inspiración para las nuevas generaciones, mostrando que con determinación y amor por lo que se hace, es posible dejar una huella profunda en la sociedad.'
     },
   ];
-
-  const generateContentItem = (item, index) => {
-    switch (item.type) {
-      case 'image':
-        return (
-          <article key={index} className="w-11/12 lg:w-full mx-auto lg:mx-0">
-            <Image src={item.image} alt={"image"} className="w-full object-cover"/>
-          </article>
-        )
-        break;
-      case 'quote':
-        return (
-          <article key={index}>
-            <div className="flex flex-col gap-5 px-5 md:px-10 w-10/12 mx-auto border-s-2 md:border-s-8 border-[color:var(--primary-color)]">
-              <h2 className="font-resolute text-md md:text-[27px] text-[color:var(--secondary-color)] decoration-[color:var(--primary-color)] underline underline-offset-4 md:underline-offset-[6px]">{item.quote}</h2>
-              <p className="font-appliedSans text-sm md:text-[25px] leading-[17px] md:leading-8">{item.author}</p>
-            </div>
-          </article>
-        )
-        break;
-      default:
-        return (
-          <article key={index} className="relative">
-            <div className="flex flex-col gap-5 px-10 md:px-20">
-              <h2 className="font-resolute lg:w-max text-md md:text-[27px] text-[color:var(--secondary-color)] decoration-[color:var(--primary-color)] underline underline-offset-4 md:underline-offset-[6px]">{item.title}</h2>
-              <p className="font-appliedSans text-sm md:text-[25px] leading-[17px] md:leading-8">{item.excerpt}</p>
-            </div>
-          </article>
-        )
-        break;
-    }
-  };
 
   return(
     <>
