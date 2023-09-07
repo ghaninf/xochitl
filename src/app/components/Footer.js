d2468b9cbf8360b85b19ed336a90b504901c8ed1
abcd123 <ghaninf@gmail.com>
2023-09-07 14:09:37 +0700

    fix footer link

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,17 +6,19 @@ import FacebookIcon from '../assets/image/socmed/facebook.png';
 import TwitterIcon from '../assets/image/socmed/twitter.png';
 import InstagramIcon from '../assets/image/socmed/instagram.png';
 import YoutubeIcon from '../assets/image/socmed/youtube.png';
-import DiscordIcon from '../assets/image/socmed/discord.png';
+import TiktokIcon from '../assets/image/socmed/tiktok.png';
+// import DiscordIcon from '../assets/image/socmed/discord.png';
 import Logo from "./Logo";
 import FooterLogo from "./FooterLogo";
 
 export default function Footer ({borderStyle = ""}) {
   const socMeds = [
-    { src: FacebookIcon, width: "w-[10.03px] md:w-[14.18px]", height: "h-[19.11px] md:h-[27.01px]", href: "https://www.facebook.com"},
-    { src: TwitterIcon, width: "w-[21.73px] md:w-[30.72px]", height: "h-[17.66px] md:h-[24.96px]", href: "https://www.twitter.com"},
-    { src: InstagramIcon, width: "w-[19.11px] md:w-[27.01px]", height: "h-[19.11px] md:h-[27.01px]", href: "https://www.instagram.com" },
-    { src:YoutubeIcon, width: "w-[29px] md:w-[41px]", height: "h-[29px] md:h-[41px]", href: "https://www.youtube.com" },
-    { src: DiscordIcon, width: "w-[22.63px] md:w-[32px]", height: "h-[17.25px] md:h-[24.39px]", href: "https://www.discord.com" },
+    { src: FacebookIcon, width: "w-[10.03px] md:w-[14.18px]", height: "h-[19.11px] md:h-[27.01px]", href: "https://www.facebook.com/Xochitl.Galvez.R/", target: '_blank', rel: 'noopener'},
+    { src: TwitterIcon, width: "w-[21.73px] md:w-[30.72px]", height: "h-[17.66px] md:h-[24.96px]", href: "https://twitter.com/XochitlGalvez", target: '_blank', rel: 'noopener'},
+    { src: InstagramIcon, width: "w-[19.11px] md:w-[27.01px]", height: "h-[19.11px] md:h-[27.01px]", href: "https://www.instagram.com/XochitlGalvez/" , target: '_blank', rel: 'noopener'},
+    { src: YoutubeIcon, width: "w-[29px] md:w-[41px]", height: "h-[29px] md:h-[41px]", href: "https://www.youtube.com/@XochitlGalvezR" , target: '_blank', rel: 'noopener'},
+    { src: TiktokIcon, width: "w-6 md:w-8", height: "h-6 md:h-8", href: "https://www.tiktok.com/@xochitlgalvezr"  , target: '_blank', rel: 'noopener'},
+    // { src: DiscordIcon, width: "w-[22.63px] md:w-[32px]", height: "h-[17.25px] md:h-[24.39px]", href: "https://www.discord.com" , target: '_blank', rel: 'noopener'},
   ];
 
   return (
@@ -31,10 +33,10 @@ export default function Footer ({borderStyle = ""}) {
             />
           </div>
           <ul className="flex flex-col gap-5 text-sm md:text-[18px]">
-            <li><Link href="/" className="font-bold text-[color:var(--secondary-color)]">XÓCHITL</Link></li>
+            <li><Link href="/xochitl" className="font-bold text-[color:var(--secondary-color)]">XÓCHITL</Link></li>
             <li><Link href="/propuestas" className="font-bold text-[color:var(--secondary-color)]">PROPUESTAS</Link></li>
             <li><Link href="/eventos" className="font-bold text-[color:var(--secondary-color)]">EVENTOS</Link></li>
-            <li><Link href="/tienda" className="font-bold text-[color:var(--secondary-color)]">TIENDA</Link></li>
+            <li><Link href="https://www.xochitl.io/" className="font-bold text-[color:var(--secondary-color)]" target="_blank" rel="noopener">TIENDA</Link></li>
           </ul>
         </div>
         <div className="basis-1/2 lg:basis-1/3 flex flex-col gap-5 md:mt-5">
@@ -46,9 +48,9 @@ export default function Footer ({borderStyle = ""}) {
             />
           </div>
           <ul className="flex flex-col gap-3 text-sm md:text-[15px]">
-            <li><Link href="/politica-de-privacidad">Política de Privacidad</Link></li>
-            <li><Link href="/terminos-y-conditiones">Términos y Condiciones</Link></li>
-            <li><Link href="/contacto">Contacto</Link></li>
+            <li><Link href="https://www.xochitl.io/policies/privacy-policy" target="_blank" rel="noopener">Política de Privacidad</Link></li>
+            <li><Link href="https://www.xochitl.io/policies/terms-of-service" target="_blank" rel="noopener">Términos y Condiciones</Link></li>
+            <li><Link href="https://www.xochitl.io/pages/contacto" target="_blank" rel="noopener">Contacto</Link></li>
           </ul>
         </div>
       </div>
