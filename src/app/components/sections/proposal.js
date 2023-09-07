e0bd5839c10e4808916e790fb272d95c9e02791b
abcd123 <ghaninf@gmail.com>
2023-09-07 11:44:30 +0700

    change text active behavior

--- src/app/components/sections/proposal.js
+++ src/app/components/sections/proposal.js
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 
 import Icon from "../icon/Icon";
 import { ArrowText } from "../text";
@@ -9,35 +8,22 @@ import { handleRichText } from "../../utils";
 import IconExpand from '../../assets/image/vector-expand.svg';
 
 export default function ProposalSection() {
-  const router = useRouter();
-  const [menu, setMenu] = useState({
+  const menu = {
     menu1: [
-      { title: 'Xóchitlnomics', expand: false, description: 'México es uno de los países más desiguales del mundo. Sin duda, uno de los principales problemas de nuestro país es la distribución de la riqueza en pocas manos.\n\nPara combatir esto primero necesitamos tener un Estado de Derecho, la ley es la ley y la deben de cumplir todos sin excepción alguna.\n\nNecesitamos recuperar la paz y la tranquilidad eliminando por completo las extorciones por parte de los funcionarios públicos.\n\nTenemos que darle todas las herramientas necesarias a las nuevas generaciones para que destaquen profesionalmente, así como crear las condiciones necesarias para su desarrollo óptimo.'},
-      { title: 'Sistema Universal de Salud', expand: false, description: 'Necesitamos que cualquier persona, sin importar si está en el seguro social o si tiene trabajo forma\u{2013}pueda acceder a un hospital público que tenga infraestructura de calidad.\n\nDe la misma manera, necesitamos un suministro basto y suficiente de medicamentos, incluyendo los que son de cadena fría.' },
-      { title: 'Internet para todo México', expand: false, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
-      { title: 'Vivienda', expand: false, description: 'Que todas las personas tengan acceso a una vivienda digna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nUt risus dui, ultrices nec ultrices non, tincidunt a tellus. Proin suscipit tortor magna, ut volutpat sem malesuada eget.' },
-      { title: 'Seguridad', expand: false, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
+      { id: 'menu1', title: 'Xóchitlnomics', description: 'México es uno de los países más desiguales del mundo. Sin duda, uno de los principales problemas de nuestro país es la distribución de la riqueza en pocas manos.\n\nPara combatir esto primero necesitamos tener un Estado de Derecho, la ley es la ley y la deben de cumplir todos sin excepción alguna.\n\nNecesitamos recuperar la paz y la tranquilidad eliminando por completo las extorciones por parte de los funcionarios públicos.\n\nTenemos que darle todas las herramientas necesarias a las nuevas generaciones para que destaquen profesionalmente, así como crear las condiciones necesarias para su desarrollo óptimo.'},
+      { id: 'menu2', title: 'Sistema Universal de Salud', description: 'Necesitamos que cualquier persona, sin importar si está en el seguro social o si tiene trabajo forma\u{2013}pueda acceder a un hospital público que tenga infraestructura de calidad.\n\nDe la misma manera, necesitamos un suministro basto y suficiente de medicamentos, incluyendo los que son de cadena fría.' },
+      { id: 'menu3', title: 'Internet para todo México', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
+      { id: 'menu4', title: 'Vivienda', description: 'Que todas las personas tengan acceso a una vivienda digna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nUt risus dui, ultrices nec ultrices non, tincidunt a tellus. Proin suscipit tortor magna, ut volutpat sem malesuada eget.' },
+      { id: 'menu5', title: 'Seguridad', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
     ],
     menu2: [
-      { title: 'Empleo', expand: false, description: 'Seguro de vida para todos aquellos que trabajan. Habilidades tecnológicas a todos los jóvenes para que accedan a mejores empleos.' },
-      { title: 'Cambio climático', expand: false, description: 'México podría ser una potencia mundial en energía limpia.\n\nTenemos cientos de litorales donde podríamos desalar el agua y con energía solar producir hidrógeno verde, que es lo que va a mover la industria automotriz del futuro.' },
-      { title: 'Proteccion de animales', expand: false, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
-      { title: 'Equidad de género', expand: false, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
+      { id: 'menu6', title: 'Empleo', description: 'Seguro de vida para todos aquellos que trabajan. Habilidades tecnológicas a todos los jóvenes para que accedan a mejores empleos.' },
+      { id: 'menu7', title: 'Cambio climático', description: 'México podría ser una potencia mundial en energía limpia.\n\nTenemos cientos de litorales donde podríamos desalar el agua y con energía solar producir hidrógeno verde, que es lo que va a mover la industria automotriz del futuro.' },
+      { id: 'menu8', title: 'Proteccion de animales', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
+      { id: 'menu9', title: 'Equidad de género', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut risus dui, ultrices nec ultrices non, tincidunt a tellus.\n\nProin suscipit tortor magna, ut volutpat sem malesuada eget.' },
     ]
-  })
-
-  const handleMenu = (menuArr, index) => {
-    let listMenu = menu[menuArr]
-    if (listMenu[index]?.href) {
-      router.push(listMenu[index]?.href)
-      return
-    }
-    listMenu[index].expand = !listMenu[index].expand
-    setMenu(prev => ({
-      ...prev,
-      [menuArr]: listMenu
-    }))
-  }
+  };
+  const [active, setActive] = useState('');
 
   return(
     <section className="relative w-full h-auto 2xl:px-[22%] xl:px-44 lg:px-28 md:px-20 px-16 lg:py-40 py-20">
@@ -50,17 +36,17 @@ export default function ProposalSection() {
           {
             menu.menu1.map((el, key) => (
               <li key={key} className="max-w-full md:max-w-[476px]" >
-                <div onClick={() => handleMenu('menu1' ,key)} className='inline-flex items-center gap-x-3 lg:gap-x-4 w-fit cursor-pointer'>
+                <div onClick={() => setActive(el.id === active ? '' : el.id)} className='inline-flex items-center gap-x-3 lg:gap-x-4 w-fit cursor-pointer'>
                   <Icon
                     src={IconExpand}
                     width={11}
                     height={16}
                     title={'Xóchitlnomics'}
-                    additionalCSS={`max-h-3 lg:max-h-4 max-w-[8px] lg:max-w-[11px] ease-in-out duration-200 ${el?.expand ? 'rotate-90' : ''}`}
+                    additionalCSS={`max-h-3 lg:max-h-4 max-w-[8px] lg:max-w-[11px] ease-in-out duration-200 ${el.id === active ? 'rotate-90' : ''}`}
                   />
-                  <span className={`${el?.expand ? 'before:w-0' : 'before:w-full'} relative before:border-b-2 before:border-pink-500 before:absolute before:-bottom-1 before:left-0 before:transform before:duration-200`}>{el.title}</span>
+                  <span className={`${el.id === active ? 'before:w-0' : 'before:w-full'} relative before:border-b-2 before:border-pink-500 before:absolute before:-bottom-1 before:left-0 before:transform before:duration-200`}>{el.title}</span>
                 </div>
-                <div className={`${el.expand ? 'scale-100 h-auto' : 'h-0 scale-y-0'} relative mt-2 sm:mt-3 lg:mt-4 pl-5 lg:pl-7 max-w-full md:max-w-sm lg:max-w-md transform ease-in-out duration-300`}>
+                <div className={`${el.id === active ? 'scale-100 h-auto' : 'h-0 scale-y-0'} relative mt-2 sm:mt-3 lg:mt-4 pl-5 lg:pl-7 max-w-full md:max-w-sm lg:max-w-md transform ease-in-out duration-300`}>
                   {handleRichText(el.description)}
                 </div>
               </li>
@@ -71,17 +57,17 @@ export default function ProposalSection() {
           {
             menu.menu2.map((el, key) => (
               <li key={key} className="max-sm:max-w-full max-w-[476px]" >
-                <div onClick={() => handleMenu('menu2', key)} className='inline-flex items-center gap-x-4 w-fit cursor-pointer'>
+                <div onClick={() => setActive(el.id === active ? '' : el.id)} className='inline-flex items-center gap-x-4 w-fit cursor-pointer'>
                   <Icon
                     src={IconExpand}
                     width={11}
                     height={16}
                     title={'Xóchitlnomics'}
-                    additionalCSS={`max-h-3 lg:max-h-4 max-w-[8px] lg:max-w-[11px] ${el?.expand ? 'rotate-90' : ''}`}
+                    additionalCSS={`max-h-3 lg:max-h-4 max-w-[8px] lg:max-w-[11px] ${el.id === active ? 'rotate-90' : ''}`}
                   />
-                  <span className={`${el?.expand ? 'before:w-0' : 'before:w-full'} relative before:border-b-2 before:border-pink-500 before:absolute before:-bottom-1 before:left-0 before:transform before:duration-200`}>{el.title}</span>
+                  <span className={`${el.id === active ? 'before:w-0' : 'before:w-full'} relative before:border-b-2 before:border-pink-500 before:absolute before:-bottom-1 before:left-0 before:transform before:duration-200`}>{el.title}</span>
                 </div>
-                <div className={`${el.expand ? 'scale-100 h-auto' : 'h-0 scale-y-0'} relative mt-2 sm:mt-3 lg:mt-4 pl-5 lg:pl-7 max-w-full md:max-w-sm lg:max-w-md transform ease-in-out duration-300`}>
+                <div className={`${el.id === active ? 'scale-100 h-auto' : 'h-0 scale-y-0'} relative mt-2 sm:mt-3 lg:mt-4 pl-5 lg:pl-7 max-w-full md:max-w-sm lg:max-w-md transform ease-in-out duration-300`}>
                   {handleRichText(el.description)}
                 </div>
               </li>
@@ -91,7 +77,7 @@ export default function ProposalSection() {
             title={'VER TODAS'}
             color={'font-secondary'}
             arrowColor={'blue'}
-            href={'/'}
+            href={'/eventos'}
             width={30}
             height={24}
             additionalClass={'mt-4 md:mt-20 lg:mt-28 text-sm lg:text-[30px]'}
