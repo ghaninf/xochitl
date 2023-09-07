c67076870e477b30fcee5081de8ab4bbe1a983d7
abcd123 <ghaninf@gmail.com>
2023-09-07 13:01:19 +0700

    animate + filter

--- src/app/components/form/Dropdown.js
+++ src/app/components/form/Dropdown.js
@@ -14,10 +14,26 @@ export default function Dropdown({ name, value, placeholder, data, isDate, arrow
     setState(prev => !prev)
   }
 
+  const typeValue = (value) => {
+    const type = typeof value
+    if (type === 'number') {
+      if (isDate) {
+        return moment(value).format('dddd, dd MMMM')
+      }
+      return value
+    }
+    if (type === 'string') {
+      if (value === '') {
+        return placeholder
+      }
+      return value
+    }
+  }
+
   return(
     <div className="relative">
       <button onClick={() => setState(prev => !prev)} className={`p-2 font-medium text-center inline-flex items-center uppercase gap-x-2 ${customClassButton || ''}`} type="button">
-        { value ? isDate ? moment(value).format('dddd, dd MMMM') : value : placeholder }
+        { typeValue(value) }
         <Icon
           src={IconDropDown}
           height={12}
@@ -25,12 +41,12 @@ export default function Dropdown({ name, value, placeholder, data, isDate, arrow
           additionalCSS={arrow || ''}
         />
       </button>
-      <div className={`${state ? 'block' : 'hidden'} absolute top-10 -left-6 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+      <div className={`${state ? 'block' : 'hidden'} absolute top-12 -left-6 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-44 dark:bg-gray-700`}>
         <ul className={`py-2 text-sm text-gray-700 dark:text-gray-200 ${customClassDropdown || ''}`}>
           {
             data.map((el, key) => (
               <li key={key} onClick={() => onChange(name, el)} className="block px-4 py-2 capitalize hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
-                { isDate ? moment(el).format('dddd, DD MMMM') : el }
+                { typeValue(el) }
               </li>
             ))
           }
