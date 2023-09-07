bd994a8df5f1eec3a54a097dc690adba7de3edaa
abcd123 <ghaninf@gmail.com>
2023-09-07 15:10:37 +0700

    change custom form to iframe (#25)

--- src/app/api/subscribe/route.js
+++ src/app/api/subscribe/route.js
@@ -5,6 +5,7 @@ export async function POST(req) {
   const URL = process.env.BEEHIIV_URL
   const PUB = process.env.BEEHIIV_PUB_KEY
   const KEY = process.env.BEEHIIV_API_KEY
+  const WEBSITE = process.env.NAME_WEBSITE
 
   const requestOptions = {
     method: 'POST',
@@ -17,10 +18,10 @@ export async function POST(req) {
       email: body?.email,
       reactivate_existing: false,
       send_welcome_email: false,
-      utm_source: 'localhost:3000',
+      utm_source: WEBSITE,
       utm_campaign: 'xochitl',
       utm_medium: 'organic',
-      referring_site: 'http://localhost:3000',
+      referring_site: WEBSITE,
       custom_fields: [
         {
           name: "Name",
@@ -28,7 +29,22 @@ export async function POST(req) {
         },
       ]
     }),
-  };
+  }
+  // {
+  //   "email": "ghaninf@gmail.com",
+  //   "reactivate_existing": false,
+  //   "send_welcome_email": false,
+  //   "utm_source": "WEBSITE",
+  //   "utm_campaign": "xochitl",
+  //   "utm_medium": "organic",
+  //   "referring_site": "WEBSITE",
+  //   "custom_fields": [
+  //     {
+  //       "name": "Name",
+  //       "value": "Ghani"
+  //     },
+  //   ]
+  // }
 
   const res = await fetch(`${URL}/publications/${PUB}/subscriptions`, requestOptions)
 
