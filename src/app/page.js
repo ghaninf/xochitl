34d16048309dceed209192ea7ef0b690646e8a99
hellofromiqbal <120085191+hellofromiqbal@users.noreply.github.com>
2023-09-08 19:59:27 +0700

    adjust vel calendario cta parent element (#34)

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default async function Home() {
         <BannerImage />
         <section className='relative w-full'>
           <SubscribeForm />
-          <div className='font-black min-h-[550px] max-sm:ml-[35px] max-lg:ml-[40%] max-lg:-mt-10 max-sm:max-w-[calc(100%-35px)] max-lg:max-w-[60%] p-9 lg:mb-20 lg:w-[calc(50%-20px)] lg:pl-[128px] lg:pt-[91px] lg:float-right relative box-border bg-[#ECECEC] z-20'>
+          <div className='font-black md:min-h-[550px] max-sm:ml-[35px] max-lg:ml-[40%] max-lg:-mt-10 max-sm:max-w-[calc(100%-35px)] max-lg:max-w-[60%] p-9 lg:mb-20 lg:w-[calc(50%-20px)] lg:pl-[128px] lg:pt-[91px] lg:float-right relative box-border bg-[#ECECEC] z-20'>
             <div className='mx-auto w-full sm:w-[362px]'>
               <h2 className='font-resolute tracking-[0.8px] leading-[26px] font-normal text-[#231F20] text-[22px] pb-1 border-b-[3px] border-pink-500'>Es hora de tomar acción</h2>
               <h3 className='mt-[23px] text-[18px] text-black font-version uppercase font-normal leading-normal tracking-[1.08px]'>México te necesita</h3>
